b9fe9b910c9a27412efa779ffb212044e870ec4a
Srijoni <89831977+itskutush@users.noreply.github.com>
2025-06-04 11:26:38 +0530

    Create stringConcatenation.cpp

---
+++ Day1/stringConcatenation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
+    string s1 = "Johnny is ";
+    string s2 = "going to school";
+    string s3 = s1 + s2;
+    cout<<s3;
+    
+
+}
