ce504acdc7eab86ef98abd8eb8c0ff4fa299e7c4
Srijoni <89831977+itskutush@users.noreply.github.com>
2025-06-04 11:33:55 +0530

    Create stringReversal.cpp

---
+++ Day1/stringReversal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    string s1 = "Practicing";
+    int n = s1.length();
+    for (int i = n;i>=0;i--){
+        printf("%c",s1[i]);
+    }
+
+    
+}
