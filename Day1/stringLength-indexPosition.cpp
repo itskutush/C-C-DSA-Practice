82c398b8bd049e87b5322aa1ce8f194c87e22448
Srijoni <89831977+itskutush@users.noreply.github.com>
2025-06-04 12:41:28 +0530

    Create stringLength-indexPosition.cpp

---
+++ Day1/stringLength-indexPosition.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s = "Hello World!";
+    printf("%lu\n",s.length());
+    printf("%c",s[7]);
+
+    return 0;
+}
