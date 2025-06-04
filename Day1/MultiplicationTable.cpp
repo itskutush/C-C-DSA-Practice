8e2d9027e2614547a2dfc4421c934727358161c9
Srijoni <89831977+itskutush@users.noreply.github.com>
2025-06-04 12:31:39 +0530

    Create MultiplicationTable.cpp

---
+++ Day1/MultiplicationTable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1 = 6;
+    for (int i = 1;i<=10;i++){
+        int prod = num1* i;
+        cout<<num1<<"*"<<i<<"="<<prod<<"\n";
+    }
+
+    return 0;
+}
