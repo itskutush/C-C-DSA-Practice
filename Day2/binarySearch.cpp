729fc585ab1bc2faca5ca73da71f1022713df082
Srijoni <89831977+itskutush@users.noreply.github.com>
2025-06-05 10:38:28 +0530

    Create binarySearch.cpp

---
+++ Day2/binarySearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int arr1 [5] = {1,2,99,4,5};
+    int target = 99;
+    bool flag = false;
+    for (int i = 0;i<5;i++){
+        if (arr1[i] == target){
+            flag = true;
+        }
+    }
+    
+    if (flag == true){
+        printf("Target found");
+    }
+    else {
+        printf("Target not found");
+    }
+}
+    
+//Time complexity of binary search : O(n)    
+    
+
+
+    
+    
+
+
