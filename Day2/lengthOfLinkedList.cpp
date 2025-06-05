da1308d0969d1a441c65a9874b4192ba116386e9
Srijoni <89831977+itskutush@users.noreply.github.com>
2025-06-05 13:57:54 +0530

    Create lengthOfLinkedList.cpp

---
+++ Day2/lengthOfLinkedList.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+public:
+
+    int data;
+    Node* next;
+    
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+//Creating a linked list from the array
+Node* convertArr2LL(vector<int>&arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for (int i = 1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+//Reversal of linked list
+Node* reverseLinkedList(vector<int>&arr){
+    int n = arr.size();
+    Node* tail = new Node(arr[arr.size()-1]);
+    Node* mover = tail;
+    for (int i= n;i>=0;i--){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover= temp;
+    }
+    return tail;
+}
+//Length of the Linked List
+int lengthOfLL(Node* head){
+    int count = 0;
+    Node* temp =  head;
+    while (temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
+//Time complexity of Traversal in linked list : O(n)
+//Traversal of a linked list
+int main(){
+    vector<int>arr = {1,2,3,4,5};
+    Node* head = convertArr2LL(arr);
+    Node* temp = head;
+    while (temp !=nullptr){
+        cout<<temp->data;
+        temp = temp->next;
+        
+    }
+    Node* tail = reverseLinkedList(arr);
+    cout<<tail->data<<"\n";
+    int count = lengthOfLL(head);
+    cout<<"The length of the linked list"<<count;
+    
+
+}
